c9e2b53bbc98f3c33db7f565272ef045400b1bba
Aaron T. Himelman <himelman@fc010-labpc-11.cs.princeton.edu>
2014-10-06 20:26:57 -0400

    added yield timing

--- process3.c
+++ process3.c
@@ -2,8 +2,19 @@
 #include "syslib.h"
 #include "util.h"
 
+static uint64_t inst_count;
+static bool_t count_set = FALSE;
 
 void _start(void)
 {
+    uint64_t diff = get_timer() - inst_count;
+    if (!count_set) {
+        count_set = TRUE;
+        inst_count = get_timer();
+        yield();
+    } else {
+        print_str(0, 35, "yield instruction count: ");
+        print_int(0, 60, (int) diff);
+    }
     exit();
 }
